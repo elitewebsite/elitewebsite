38b851a6de57994a914c648e910ebc49b267e56c
Muchmark <muchmarklbh@gmail.com>
2022-10-22 17:51:23 +0530

    f

--- src/Components/Singleproduct.js
+++ src/Components/Singleproduct.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import { useLocation,useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import Iframe from 'react-iframe'
 export const Singleproduct = () => {
     const { state } = useLocation()
     const navigate = useNavigate()
     useEffect(() => {
 
         if (state) {
-           
+
         }
         else {
             navigate('/')
         }
     }, [state])
 
-    console.log(state?.oneproduct)
+
+
     return (
         <div><h1>Singleproduct info</h1>
-        <h1>{state.oneproduct.product_name}</h1>
-        <h1>{state.oneproduct.model_no}</h1>
-        
-        </div>
+            <h1>{state.oneproduct.product_name}</h1>
+            <h1>{state.oneproduct.model_no}</h1>
+            <h1>{state.oneproduct.youtube}</h1>
+            <iframe width="420" height="315" frameborder="0" allowfullscreen
+                src={`https://www.youtube.com/embed/${state.oneproduct.youtube.split('=')[1]}`}>
+            </iframe>
+
+        </div >
     )
 }
